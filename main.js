72a5a2a623a960c80f42a4d8f4a3d9f8a60e8a44
carlos <bobcn_1991@hotmail.com>
2017-11-14 00:10:56 +0000

    DB deleted for now, Reestructure main.js following tutorial TraversyMedia; Test createAddWindow pop up

--- main.js
+++ main.js
@@ -1,86 +1,59 @@
-const {app, BrowserWindow, Menu} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow, Menu} = require('electron');
+const url = require('url');
+const path = require('path');
 
-//***** ----  SQLite3 ---- ******
-let server = require('./api.js')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+//App on Ready
 let win
+app.on('ready', ()=>{
+win = new BrowserWindow({width: 800, height: 600});
+win.loadURL(url.format({
+   pathname: path.join(__dirname, 'index.html'),
+   protocol:'file:',
+   slashes:true
+ }));
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-//***** ---- Main menu ---- *****
-app.on('ready', function () {
-  new BrowserWindow();
-
-  const menuTemplate = [
-    {
-        label: 'Electron',
-        submenu: [
-            {
-                label: 'About ...',
-                click: () => {
-                    console.log('About Clicked');
-                }
-            }, {
-                type: 'separator'
-            }, {
-                label: 'Quit',
-                click: () => {
-                    app.quit();
-                }
-            }
-        ]
-    }
-];
-   const menu = Menu.buildFromTemplate(menuTemplate);
-   Menu.setApplicationMenu(menu);
+ // build menu from menu template (see array line 38 'template')
+ const mainMenu = Menu.buildFromTemplate(template);
+ Menu.setApplicationMenu(mainMenu);
 });
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+// Handle createAddWindow
+let addWindow;
+function createAddWindow(){
+  addWindow = new BrowserWindow({
+    width:200,
+    height:300,
+    title: 'Check In'
+  });
+  addWindow.loadURL(url.format({
+     pathname: path.join(__dirname, 'receipt.html'),
+     protocol:'file:',
+     slashes:true
+   }));
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+//menu template
+  const template = [
+    {
+      label:'New',
+      submenu:[
+       { 
+         label: 'Check In',
+         click(){
+           createAddWindow();
+         }
+        },
+       { label: 'Check Out'},
+       { label: 'Refund'},
+       { label: 'Total shift'},
+       { label: 'Weekly Total'},
+       { label: 'Quit',
+          accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+          click(){
+            app.quit();
+          }
+        }
+      ] 
+    }
+  ];
